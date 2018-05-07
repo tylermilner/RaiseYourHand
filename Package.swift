21e9528665d9a768942941cc9921f471a7ae2100
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-05-07 17:04:06 -0500

    Updated to Vapor 3.0.0 release.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "RaiseYourHand",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
